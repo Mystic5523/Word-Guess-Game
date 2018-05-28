d34f22a54be1b156f39cc45b65a84d64fc7d1dc6
Mystic5523 <Mystic5523@hotmail.com>
2018-05-28 16:07:37 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,64 @@
-// var setup = function()
-// {
-//     for (var i = 0; i < answer.length; i++)
-//     {
-//         display[i] = "_ ";
-//         output = output + display[i];
-//     }
-//     document.getElementById("game").html = output;
-//     output = "";
-// }
+var word = [ "LORD NIKON", "PHREAK", "ACID BURN", "CRASH OVERRIDE", "CEREAL KILLER", "ZERO COOL", "THEPLAGUE"]
+    var rand = Math.floor(Math.random() * word.length)
+    var choice =  word[rand];
+    var wordLength = choice.length;
+    var display = wordLength;
+    var win = wordLength;
+    var letters = choice.split(``);
+    var guessesLeft = 10;
+    var output = " ";
+    var userGuess = " ";
+    var randWordArray = [];
+
+    document.onkeyup = function(event) {
+        if (event.keyCode >= 65 && event.keyCode <= 90){
+        userGuess(event.key);
+        }
+    }
+
+    console.log(display);
+    console.log(letters);
+    console.log(choice);
+    console.log(guessesLeft);
+    document.write(letters);
+
+    for (var i = 0; i < choice.length; i++){
+        if (choice[i] === ' ')
+        {
+        randWordArray.push('- ');
+        }
+        else {
+        randWordArray.push('_ ');
+        }
+        document.getElementById("game").textContent = randWordArray.join("");
+        console.log(choice[i]);
+    }
+
     
+       
 
-// var submit = function()
-// {
+   
+    function userGuess(letter){
+    console.log(letter);
+    console.log(choice.indexOf(letter));
+    if (randomWord.indexOf(letter >= 0)) {
+        for (var i = 0; i < randomWord.length; i++);{
+            if(choice[i] == letter){
+            randWordArray[i] = choice[i];  
+            }
+        }
+    document.getElementById("game").textContent = randWordArray.join("");
+    }
+    else {
+        // wrong letter adds +1 to wrong counter
+        if (randomWord.indexOf(letter === -1)) {
+            guessesLeft--;
+            console.log(guessesLeft);
+            console.log(letter);
+        }
+    }
+    }
 
-//     output = "";
-//     userGuess = $("letter").value;
-//     $("letter").value = "";
 
-//     for (var c= 0; c , answer.legnth; c++)
-//     {
-//         alert(letters[c]);
-//         if (userGuess.toUpperCase() == letters[c])
-//         {
-//             display[c] = userGuess.toUpperCase();
-//             win--;
-//         }
-//             output = output + display[c] + " ";
-//     }
-//     doxument.getElementById("game").innerHTML = output;
-//     output="";
-//     guessesLeft--;
-
-//     if (win < 1)
-//     {
-//         document.getElementById("guesses").innerHTML = "YOU WIN!";
-//     }
-//     else if (guessesLeft < 1)
-//     {
-//         document.getElementById("guesses").innerHTML = "You have " + guessLeft + " guesses left.";
-//     }
-
-//     document.querySelector("#game").innerHTML = html;
-// }
-
-// window.onload = function()
-// {
-//     setup();
-//     $("submit").onclock = submit;
-// }
+ 
+   
